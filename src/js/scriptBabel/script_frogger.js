b720d062327989dedc8d75b095fe706cc0713649
Andrey <andrey@fattoriaweb.com.br>
2017-12-21 17:34:38 -0200

    começando projeto

--- src/js/scriptBabel/script_frogger.js
+++ src/js/scriptBabel/script_frogger.js
@@ -3,4 +3,44 @@
  * Desenvolvido por Andrey em: 21/12/2017
  */
 
-() => console.log('teste');
+class Game {
+    constructor(){
+        this.actualStage = 1;
+        this.stage();
+    }
+    stage(){
+        switch(this.actualStage){
+            case 1:
+                this.lines(4);
+                break;
+            case 2:
+                this.lines(8);
+                break;
+            case 3:
+                this.lines(12);
+                break;
+            case 4 :
+                this.lines(16);
+        }
+    }
+    lines(quantity){
+
+    }
+}
+
+class Lines extends Game{
+    constructor(){}
+
+    templateStreet(){
+        const streetDiv = document.createElement('div');
+
+        streetDiv.setAttribute('class', 'line street');
+        streetDiv.appendChild(new Enemy('car'));
+    }
+}
+
+class Enemy extends Game{
+    constructor(type){
+        this.type = type;
+    }
+}
--- src/js/script_frogger.js
+++ src/js/script_frogger.js
@@ -1,10 +1,40 @@
-'use strict';
+"use strict";
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 /*!
  * Frogger
  * Desenvolvido por Andrey em: 21/12/2017
  */
 
-(function () {
-  return console.log('teste');
-});
+var Game = function () {
+    function Game() {
+        _classCallCheck(this, Game);
+
+        this.actualStage = 1;
+        this.stage();
+    }
+
+    _createClass(Game, [{
+        key: "stage",
+        value: function stage() {
+            switch (this.actualStage) {
+                case 1:
+                    this.lines(4);
+                    break;
+                case 2:
+                    this.lines(8);
+                    break;
+                case 3:
+                    this.lines(12);
+                    break;
+                case 4:
+                    this.lines(16);
+            }
+        }
+    }]);
+
+    return Game;
+}();
