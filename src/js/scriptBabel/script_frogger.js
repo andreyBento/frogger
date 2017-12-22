10fa6a7649f3b8f9a64fb74fc23a611df2df3818
Andrey <andrey@fattoriaweb.com.br>
2017-12-22 16:07:33 -0200

    ajustando gerador de linhas

--- src/js/scriptBabel/script_frogger.js
+++ src/js/scriptBabel/script_frogger.js
@@ -29,7 +29,26 @@ class Game {
 }
 
 class Lines extends Game{
-    constructor(){}
+    constructor(type){
+        this.type = type;
+        this.generate();
+    }
+
+    generate(){
+        switch(this.type){
+            case 'street':
+                this.templateStreet();
+                break;
+            case 'river':
+                this.templateRiver();
+                break;
+            case 'pavement':
+                this.templatePavement();
+                break;
+            case 'grass':
+                this.templateGrass();
+        }
+    }
 
     templateStreet(){
         const streetDiv = document.createElement('div');
@@ -37,10 +56,38 @@ class Lines extends Game{
         streetDiv.setAttribute('class', 'line street');
         streetDiv.appendChild(new Enemy('car'));
     }
+
+    templateRiver(){
+        const riverDiv = document.createElement('div');
+
+        riverDiv.setAttribute('class', 'line river');
+        riverDiv.appendChild(new Platform());
+    }
+
+    templatePavement(){
+        const pavementDiv = document.createElement('div');
+
+        pavementDiv.setAttribute('class', 'line pavement');
+    }
+
+    templateGrass(){
+        const grassDiv = document.createElement('div');
+
+        grassDiv.setAttribute('class', 'line grass');
+    }
 }
 
 class Enemy{
     constructor(type){
         this.type = type;
+        this.direction = 0;
+        this.speed = 0;
+    }
+}
+
+class Platform{
+    constructor(){
+        this.direction = 0;
+        this.speed = 0;
     }
 }
